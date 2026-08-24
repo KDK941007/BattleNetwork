c6e950ef108ccc190281e23bd3791598d7dd9aa8
KDK941007 <y3001k.poppin@gmail.com>
2026-08-24 16:16:41 +0900

    テストチップの関連データを追加

--- js/master/chip-relations.js
+++ js/master/chip-relations.js
@@ -9,7 +9,11 @@
     {chipId:'CHIP_0003',attributeId:'NORMAL',displayPriority:2,primaryFlg:false},
     {chipId:'CHIP_0004',attributeId:'NORMAL',displayPriority:1,primaryFlg:true},
     {chipId:'CHIP_0005',attributeId:'RECOVERY',displayPriority:1,primaryFlg:true},
-    {chipId:'CHIP_0005',attributeId:'NORMAL',displayPriority:2,primaryFlg:false}
+    {chipId:'CHIP_0005',attributeId:'NORMAL',displayPriority:2,primaryFlg:false},
+    {chipId:'TEST_9001',attributeId:'SWORD',displayPriority:1,primaryFlg:true},
+    {chipId:'TEST_9001',attributeId:'NORMAL',displayPriority:2,primaryFlg:false},
+    {chipId:'TEST_9002',attributeId:'NORMAL',displayPriority:1,primaryFlg:true},
+    {chipId:'TEST_9003',attributeId:'NORMAL',displayPriority:1,primaryFlg:true}
   ];
 
   const CHIP_CODE_RELATION=[
@@ -25,7 +29,10 @@
     {chipId:'CHIP_0004',codeId:'L'},
     {chipId:'CHIP_0004',codeId:'*'},
     {chipId:'CHIP_0005',codeId:'A'},
-    {chipId:'CHIP_0005',codeId:'L'}
+    {chipId:'CHIP_0005',codeId:'L'},
+    {chipId:'TEST_9001',codeId:'M'},
+    {chipId:'TEST_9002',codeId:'G'},
+    {chipId:'TEST_9003',codeId:'D'}
   ];
 
   const CHIP_VALUE_RELATION=[
@@ -33,10 +40,15 @@
     {chipId:'CHIP_0002',valueNo:1,valueTypeId:'DAMAGE',value:80,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
     {chipId:'CHIP_0003',valueNo:1,valueTypeId:'DAMAGE',value:80,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
     {chipId:'CHIP_0004',valueNo:1,valueTypeId:'DAMAGE',value:50,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
-    {chipId:'CHIP_0005',valueNo:1,valueTypeId:'RECOVERY',value:10,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null}
+    {chipId:'CHIP_0005',valueNo:1,valueTypeId:'RECOVERY',value:10,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
+    {chipId:'TEST_9001',valueNo:1,valueTypeId:'DAMAGE',value:100,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
+    {chipId:'TEST_9002',valueNo:1,valueTypeId:'DAMAGE',value:200,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null},
+    {chipId:'TEST_9003',valueNo:1,valueTypeId:'DAMAGE',value:150,valueMode:'FIXED',displayOrder:1,displayFlg:true,labelOverride:null}
   ];
 
-  const CHIP_SPECIAL_TYPE_RELATION=[];
+  const CHIP_SPECIAL_TYPE_RELATION=[
+    {chipId:'TEST_9003',specialTypeId:'DARK'}
+  ];
 
   const CHIP_RANGE_PARAM_RELATION=[
     {chipId:'CHIP_0001',paramId:'DISTANCE',paramValue:900},
@@ -45,7 +57,12 @@
     {chipId:'CHIP_0003',paramId:'DISTANCE',paramValue:155},
     {chipId:'CHIP_0003',paramId:'WIDTH',paramValue:500},
     {chipId:'CHIP_0004',paramId:'THROW_DISTANCE',paramValue:430},
-    {chipId:'CHIP_0004',paramId:'RADIUS',paramValue:115}
+    {chipId:'CHIP_0004',paramId:'RADIUS',paramValue:115},
+    {chipId:'TEST_9001',paramId:'DISTANCE',paramValue:155},
+    {chipId:'TEST_9001',paramId:'WIDTH',paramValue:75},
+    {chipId:'TEST_9002',paramId:'DISTANCE',paramValue:900},
+    {chipId:'TEST_9003',paramId:'THROW_DISTANCE',paramValue:430},
+    {chipId:'TEST_9003',paramId:'RADIUS',paramValue:115}
   ];
 
   const CHIP_BEHAVIOR_PARAM_RELATION=[
