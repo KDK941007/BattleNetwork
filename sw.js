db0f9c0bcca8fffe02e37d8ce0d0c1110afc7fab
KDK941007 <y3001k.poppin@gmail.com>
2026-08-25 03:48:53 +0900

    Cache logical field grid renderer assets

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'battlenetwork-runtime-v38';
+const CACHE_NAME = 'battlenetwork-runtime-v39';
 const OFFLINE_URL = './index.html';
 const STATIC_ASSETS = [
   OFFLINE_URL,
   './css/style.css',
   './css/font.css',
+  './css/field-grid.css',
   './css/chip-detail.css',
   './css/chip-detail-title-extension.css',
   './css/chip-detail-range-fix.css',
@@ -19,6 +20,7 @@ const STATIC_ASSETS = [
   './js/ui/chip-detail-ui.js',
   './js/ui/chip-theme-ui.js',
   './js/field/field-grid.js',
+  './js/field/field-grid-renderer.js',
   './js/ui/test-chip-ui.js',
   './js/game.js',
   './js/service-worker-register.js',
