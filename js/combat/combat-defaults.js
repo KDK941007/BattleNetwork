06dea02ad81061058877b3762646cd518d5d1a7d
KDK941007 <y3001k.poppin@gmail.com>
2026-08-27 23:15:53 +0900

    Add default enemy approach stop distance

--- js/combat/combat-defaults.js
+++ js/combat/combat-defaults.js
@@ -2,7 +2,8 @@
   const DEFAULTS=Object.freeze({
     fullSyncWindowMs:180,
     enemyAttackRecoveryMs:250,
-    attackCooldownMs:3000
+    attackCooldownMs:3000,
+    enemyApproachStopTiles:1
   });
   window.BattleNetworkCombatDefaults=DEFAULTS;
 })();
