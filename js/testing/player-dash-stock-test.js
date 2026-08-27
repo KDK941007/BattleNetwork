907294ae4b782f7295d0799f2a1d62edc1aeb5f6
KDK941007 <y3001k.poppin@gmail.com>
2026-08-28 00:18:06 +0900

    Remove unused dash stock test helper

--- js/testing/player-dash-stock-test.js
+++
@@ -1,5 +0,0 @@
-(()=>{
-  const PARAMS=window.BattleNetworkParameters;
-  if(!PARAMS)throw new Error('BattleNetworkDashStockTest: parameter system is not loaded.');
-  PARAMS.setAdditive('player','TEST_DASH_STOCK',{dashConsecutiveCount:1});
-})();
