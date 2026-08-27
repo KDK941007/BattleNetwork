5eb642590d524343940ecbf2e090fc2afaa71b41
KDK941007 <y3001k.poppin@gmail.com>
2026-08-28 03:20:44 +0900

    Add enemy1 shockwave damage default

--- js/combat/enemy1-runtime.js
+++ js/combat/enemy1-runtime.js
@@ -7,7 +7,7 @@
   const debugListeners=new Set();
   const CHASE_POLICY=Object.freeze({ALWAYS_WHILE_AWARE:'ALWAYS_WHILE_AWARE',OVERLAP_COOLDOWN_CHASE:'OVERLAP_COOLDOWN_CHASE'});
   const CHASE_DISTANCE_MODE=Object.freeze({APPROACH:'APPROACH',KEEP_BAND:'KEEP_BAND'});
-  const ATTACK_DEFAULTS=Object.freeze({projectileSpeed:520});
+  const ATTACK_DEFAULTS=Object.freeze({projectileSpeed:520,damage:10});
   const enemyConfig=Object.freeze({chasePolicy:CHASE_POLICY.OVERLAP_COOLDOWN_CHASE,chaseDistanceMode:CHASE_DISTANCE_MODE.APPROACH,perceptionStartTiles:5,perceptionReleaseTiles:8,chaseRangeTiles:8,approachStopTiles:DEFAULTS.enemyApproachStopTiles,keepDistanceMinTiles:null,keepDistanceMaxTiles:null});
   let patternIndex=0;
   let debugState={enabled:false,showPerception:true,showAttackGlow:true};
