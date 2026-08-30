df60c1cec358f2fc9bc99d1ee41a9e9ea7cf0570
KDK941007 <y3001k.poppin@gmail.com>
2026-08-30 23:59:16 +0900

    Switch temporary custom folder to AirShot only

--- js/master/folder-service.js
+++ js/master/folder-service.js
@@ -1,8 +1,8 @@
 (()=>{
   const data=window.BattleNetworkData||{};
   const STORAGE_KEY='battleNetworkEquippedFolderId';
-  const TEST_ONLY_CANNON=true;
-  const CANNON_CODES=Object.freeze(['A','B','C']);
+  const TEST_ONLY_AIRSHOT=true;
+  const AIRSHOT_CODES=Object.freeze(['A','S','V','*']);
   const LEGACY_TYPE_BY_CHIP_ID=Object.freeze({
     CHIP_0001:'CANNON',
     CHIP_0002:'SWORD',
@@ -46,19 +46,19 @@
     return getFolder(folderId);
   }
 
-  function buildCannonTestCards(folderId){
+  function buildAirShotTestCards(folderId){
     return Array.from({length:30},(_,index)=>({
       id:index,
-      type:'CANNON',
-      code:CANNON_CODES[index%CANNON_CODES.length],
-      chipId:'CHIP_0001',
+      type:'AIRSHOT',
+      code:AIRSHOT_CODES[index%AIRSHOT_CODES.length],
+      chipId:'CHIP_EXE4_S004',
       folderId,
       slotNo:index+1
     }));
   }
 
   function toLegacyCards(folderId=getEquippedFolderId()){
-    if(TEST_ONLY_CANNON)return buildCannonTestCards(folderId);
+    if(TEST_ONLY_AIRSHOT)return buildAirShotTestCards(folderId);
     return getFolderEntries(folderId).map((entry,index)=>({
       id:index,
       type:LEGACY_TYPE_BY_CHIP_ID[entry.chipId]||`CHIP_${entry.chipId}`,
