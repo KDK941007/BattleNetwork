416f37c5ba3ad0cfea0d521aa764c12ddc8379b3
KDK941007 <y3001k.poppin@gmail.com>
2026-09-12 22:00:02 +0900

    Temporarily set player HP to 999 for Kokoro verification

--- js/combat/player-health.js
+++ js/combat/player-health.js
@@ -1,5 +1,6 @@
 (()=>{
-  const INITIAL_MAX_HP=100;
+  // Temporary Kokoro Window verification setting. Restore after Kokoro state checks are complete.
+  const INITIAL_MAX_HP=999;
   let state={maxHp:INITIAL_MAX_HP,hp:INITIAL_MAX_HP};
   const listeners=new Set();
 
